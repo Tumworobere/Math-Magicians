a7296312d219a5d29210591816c6c2d036bb1c16
Tumworobere <annaht.annah@gmail.com>
2022-01-12 22:05:26 +0300

    Add Quotes in QuoteData.js

---
+++ src/components/QuoteData.js
@@ -0,0 +1,36 @@
+const QuoteData = () => {
+  const quotes = [
+    {
+      quote: 'Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding.',
+      author: 'William Paul Thurston, American mathematician',
+    },
+    {
+      quote: 'Mathematics is the most beautiful and most powerful creation of the human spirit..',
+      author: 'Stefan Banach, Polish mathematician',
+    },
+    {
+      quote: 'I’ve always enjoyed mathematics. It is the most precise and concise way of expressing an idea.',
+      author: 'N.R. Narayana Murthy, Indian IT industrialist',
+    },
+    {
+      quote: 'Mathematics has beauty and romance. Its not a boring place to be, the mathematical world. It’s an extraordinary place; it’s worth spending time there.',
+      author: 'Marcus du Sautoy, British mathematician',
+    },
+    {
+      quote: 'SThe pure mathematician, like the musician, is a free creator of his world of ordered beauty.',
+      author: 'Bertrand Russell, British philosopher',
+    },
+    {
+      quote: 'Pure mathematics is the worlds best game. It is more absorbing than chess, more of a gamble than poker, and lasts longer than Monopoly. Its free. It can be played anywhere — Archimedes did it in a bathtub.',
+      author: 'Richard J. Trudeau, mathematician',
+    },
+    {
+      quote: 'There are two ways to do great mathematics. The first is to be smarter than everybody else. The second way is to be stupider than everybody else — but persistent.',
+      author: 'Raoul Bott, Hungarian-American mathematician',
+    },
+  ];
+
+  return quotes;
+};
+
+export default QuoteData;
