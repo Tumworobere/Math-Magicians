678651e61403f5e75e08b74672fd7be23594f511
Tumworobere <annaht.annah@gmail.com>
2023-04-13 11:33:58 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
             <Home />
           </Route>
         </Switch>
-        <Footer />
+        <footer> Powered by Annah Tumworobere Kamara</footer>
       </main>
     );
   }
